3944314a59e71040db48f0bd75a66c9baa1e1b2b
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-05-02 19:54:52 -0300

    mexendo na index

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
-koe
+class Usuario():
+    def __init__(self, nomeUsuario, senha, album):
+        self._nomeUsuario= nomeUsuario
+        self._senha= senha
+        self._album='?'
+    def getnome(self):
+        return self._nomeUsuario
+        
