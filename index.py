4bda996073572cd16c00b081b4749ab725998005
PedroLamb <pedrohenriquelamb@hotmail.com>
2023-05-02 20:41:28 -0300

    removendo arquivo index

--- index.py
+++
@@ -1,9 +0,0 @@
-class Usuario():
-    def __init__(self, nomeUsuario, senha, album):
-        self._nomeUsuario= nomeUsuario
-        self._senha= senha
-        self._album='?'
-    def getnome(self):
-        return self._nomeUsuario
-    
-print('alo mundo')
