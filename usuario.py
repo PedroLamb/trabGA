bb1bc901c59bebd5d3188fecb9d09514a8178415
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-05-02 21:42:57 -0300

    classe usuario finalizada

--- usuario.py
+++ usuario.py
@@ -7,11 +7,22 @@ class Usuario():
         self._senha= senha  #adicionar limitador
     def getnome(self):
         return self._nomeUusario
+    def getsenha(self):
+        return self._senha
+    def login(self):
+        dono= '0'
+        senhaCadastrada= '0'
+        while dono!=self._nomeUsuario or senhaCadastrada!= self._senha:
+            dono=input('Digite seu nome de usuario: ')
+            senhaCadastrada= input('Digite sua senha:')
+
+
 
 nome= input('Informe seu nome: ')   
 senha=input('Digite sua senha: ')         
 a= Usuario(nome, senha)
 a.cadastrar(nome,senha)
-print(a.getnome())            
+print(a.getnome()) 
+print(a.getsenha())     
+a.login()      
 
-print()
