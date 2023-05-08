99f1ed3496ee9a1bec510bd2fd22f7427439392c
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-05-07 21:14:30 -0300

    Create Album.py

---
+++ Album.py
@@ -0,0 +1,13 @@
+import Figurinha
+import csv
+class Album():
+    def __init__(self,paginas, figurinhas, requerimento_de_troca):
+        self._paginas= paginas
+        self._figurinhas= figurinhas
+        self._requerito_de_troca= requerimento_de_troca
+    def folhar_album(self, pagina_escolha):
+        pagina_escolha= i
+        for i in range(0,3):
+            print(i)
+    def get_figurinhas():
+        return
